b3090be0cb391e11e7c7e3834aefa9762eca9153
Vaishali <vaishali@18.com>
2026-06-10 20:37:53 +0530

    2

---
+++ multiline.java
@@ -0,0 +1,7 @@
+public class multiline {
+    public static void main(String args[]){
+        System.out.print("This is a multiline string example.");
+        System.out.println("It can span multiple lines without any issues.");
+        System.out.print("This is useful for long strings or when you want to improve readability.");
+    }
+}
