a88eba7b0f501b2599601c385e870926b3d49c65
Vaishali <vaishali@18.com>
2026-06-15 22:55:11 +0530

    6

---
+++ fors.java
@@ -0,0 +1,13 @@
+public class fors {
+    public static void main(String args[]){
+        for (int i = 0; i < 5; ++i) {
+  System.out.println(i);
+}
+int n=0;
+while(n<5){
+System.out.println(n);
+n++;
+}
+    }
+    
+}
