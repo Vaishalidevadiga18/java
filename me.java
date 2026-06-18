5d4b5740afce47be598ee905d79649e1c2ac12c7
Vaishali <vaishali@18.com>
2026-06-18 22:40:50 +0530

    10

---
+++ me.java
@@ -0,0 +1,10 @@
+public class me {
+  // Create a method named welcomeMessage
+public static void welcomeMessage(){
+System.out.println("Welcome to Java!");
+}
+  public static void main(String[] args) {
+    // Call the welcome method here
+welcomeMessage();
+  }
+}
---
+++ su.java
@@ -0,0 +1,12 @@
+public class su {
+    
+  static int myMethod(int x, int y) {
+    return x + y;
+  }
+
+  public static void main(String[] args) {
+    int a = myMethod(5, 3);
+    System.out.println(a);
+  }
+}
+// Output: 8
