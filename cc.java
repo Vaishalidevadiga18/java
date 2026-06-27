11b2fa4eb4b6b7986a655316f3447aa69832230c
Vaishali <vaishali@18.com>
2026-06-27 22:03:23 +0530

    codechef

---
+++ cc.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class cc
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc =new Scanner(System.in);
+		System.out.println("enter n values");
+		int n = sc.nextInt();
+        
+		System.out.println(2*n);
+
+	}
+}
